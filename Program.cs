ce230be8a4506e61d68ec8288bddf6b4baaf3d01
Emmanuel <erivas157@gmail.com>
2025-02-11 16:07:56 -0500

    Fixed the formatting

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using System;
 
-class Appointment {
+class Appointment 
+{
+    //Marcus
     List<string> periods = new List<string>();
 
     Appointment() {}
 
+    //Marcus
     public List<String> periods
     {
         get
@@ -15,37 +18,71 @@ class Appointment {
         {
             periods = value;
         }
-
-
-    public int isMinuteFree(int period, int minute) 
-    {
-
     }
-    public int reserveBlock(int period, int startMinute, int duration)
-    {
+    // Joseph
+    public Appointment(int startPeriod, int endPeriod) {
+        this.startPeriod = startPeriod;
+        this.endPeriod = endPeriod;
 
-    }
-    public bool makeAppointment(int startPeriod, int endPeriod, int duration) {
-        bool isBooked = false;
+        // Joseph
+        public bool isMinuteFree(int period, int minute) {
+            return period == startPeriod && minute >= startMinute && minute < 60 
+                period == endPeriod && minute >= 0 && minute < endMinute 
+                period > startPeriod && period < endPeriod;
+        // Joseph
+        public bool reserveBlock(int period, int startMinute, int duration)
+        {
+            if (isMinuteFree(period, startMinute) && isMinuteFree(period, startMinute + duration))
+            {
+            return true;
+            }
+            else
+            {
+            return false;
+            }
+        }
 
-        if (((startPeriod >= 1) && (startPeriod <= 8)) && ((endPeriod >= 1) && (endPeriod <= 8)) && ((duration >= 0) && (duration <= 59))) {
-            // Search for earliest available block of duration minutes 
-            // Use findFreeBlock(int period, int duration) - returns -1 if no block is available
-            for (int i = startPeriod; i <= endPeriod; i++) {
-                int block = -1;
-                block = findFreeBlock(i, duration);
-            // test
-                if (block != -1) {
-                    reserveBlock()
-                    isBooked = true;
+        }
+        //Marcus
+        public int findFreeBlock(int period, int duration) 
+        {
+            for (int i = 0; i < duration; i++) 
+            {
+                if (isMinuteFree(period, i))
+                {
+                    return i;
+                }
+                else 
+                {
+                    return -1;
                 }
             }
         }
 
-        return isBooked;
+        // Emmanuel
+        public bool makeAppointment(int startPeriod, int endPeriod, int duration) {
+            bool isBooked = false;
+
+            if (((startPeriod >= 1) && (startPeriod <= 8)) && ((endPeriod >= 1) && (endPeriod <= 8)) && ((duration >= 0) && (duration <= 59))) {
+                // Search for earliest available block of duration minutes 
+                // Use findFreeBlock(int period, int duration) - returns -1 if no block is available
+                for (int i = startPeriod; i <= endPeriod; i++) {
+                    int block = -1;
+                    block = findFreeBlock(i, duration);
+                // test
+                    if (block != -1) {
+                        reserveBlock()
+                        isBooked = true;
+                    }
+                }
+            }
+
+            return isBooked;
+        }
     }
 }
 
+// Emmanuel 
 class Test {
     static void main() {
         Console.WriteLine("Hello");
