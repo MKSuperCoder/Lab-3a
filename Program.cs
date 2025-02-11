015a8a0ed4b3aeb1c14542d22dbb5a5e706521e9
Marcus Swa <89757146+MKSuperCoder@users.noreply.github.com>
2025-02-11 15:04:47 -0500

    Modify functions

--- Program.cs
+++ Program.cs
@@ -2,10 +2,12 @@ using System;
 
 class Appointment 
 {
+    //Marcus
     List<string> periods = new List<string>();
 
     Appointment() {}
 
+    //Marcus
     public List<String> periods
     {
         get
@@ -17,15 +19,17 @@ class Appointment
             periods = value;
         }
     }
+    // Joseph
     public Appointment(int startPeriod, int endPeriod) {
         this.startPeriod = startPeriod;
         this.endPeriod = endPeriod;
 
+        // Joseph
         public bool isMinuteFree(int period, int minute) {
             return period == startPeriod && minute >= startMinute && minute < 60 
                 period == endPeriod && minute >= 0 && minute < endMinute 
                 period > startPeriod && period < endPeriod;
-
+        // Joseph
         public bool reserveBlock(int period, int startMinute, int duration)
         {
             if (isMinuteFree(period, startMinute) && isMinuteFree(period, startMinute + duration))
@@ -39,7 +43,7 @@ class Appointment
         }
 
         }
-
+    //Marcus
     public int findFreeBlock(int period, int duration) 
     {
         for (int i = 0; i < duration; i++) 
@@ -54,14 +58,27 @@ class Appointment
             }
         }
     }
+    //Marcus
     public bool makeAppointment(int startPeriod, int endPeriod, int duration) {
         for (int i = startPeriod; i < endPeriod; i++) 
         {
-
+            for (int minute = 0; i < duration; minute++) {
+                 if (isMinuteFree(i, duration)) 
+                {
+                    reserveBlock(i, minute, duration);
+                    return true;
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+           
         }
     }
 }
 
+// Emmunuel 
 class Test {
     static void main() {
         Console.WriteLine("Hello");
