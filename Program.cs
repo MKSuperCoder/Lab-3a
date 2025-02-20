cec4715064634afa74122c8742f62efb1cac7e05
Marcus Swa <89757146+MKSuperCoder@users.noreply.github.com>
2025-02-19 21:29:00 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,39 +9,7 @@ class Appointment
     {
         schedule = new bool[8, 60];
     }
-    // Emmanuel
-    public bool MakeAppointment(int startPeriod, int endPeriod, int duration)
-    {
-        bool isBooked = false;
-
-        if (((startPeriod >= 1) && (startPeriod <= 8)) && ((endPeriod >= 1) && (endPeriod <= 8)) && ((duration >= 0) && (duration <= 59)))
-        {
-            int startMinute = -1;
-
-            for (int period = startPeriod; i <= endPeriod; i++)
-            {
-                startMinute = findFreeBlock(period, duration);
-
-                if ((startMinute >= 0) && (startMinute <= 59))
-                {
-                    reserveBlock(period, startMinute, duration);
-                    isBooked = true;
-                    break;
-                }
-            }
-
-            if (startMinute == -1)
-            {
-                Console.WriteLine("Sorry! Unable to book appointment.");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Error! Please make sure the periods are between 1-8, and the duration is between 0-59.");
-        }
-
-        return isBooked;
-    }
+    
 
     //Marcus
     private bool isMinuteFree(int period, int minute)
@@ -86,7 +54,39 @@ class Appointment
          return -1;
       }
     } 
+     // Emmanuel
+    public bool MakeAppointment(int startPeriod, int endPeriod, int duration)
+    {
+        bool isBooked = false;
 
+        if (((startPeriod >= 1) && (startPeriod <= 8)) && ((endPeriod >= 1) && (endPeriod <= 8)) && ((duration >= 0) && (duration <= 59)))
+        {
+            int startMinute = -1;
+
+            for (int period = startPeriod; i <= endPeriod; i++)
+            {
+                startMinute = findFreeBlock(period, duration);
+
+                if ((startMinute >= 0) && (startMinute <= 59))
+                {
+                    reserveBlock(period, startMinute, duration);
+                    isBooked = true;
+                    break;
+                }
+            }
+
+            if (startMinute == -1)
+            {
+                Console.WriteLine("Sorry! Unable to book appointment.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error! Please make sure the periods are between 1-8, and the duration is between 0-59.");
+        }
+
+        return isBooked;
+    }
 }
 
 
@@ -95,6 +95,7 @@ class Test
 {
     static void Main() 
     {
+    
     }
 
     
